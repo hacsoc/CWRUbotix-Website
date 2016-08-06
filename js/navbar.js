ffcca6583468bb345a0785483e00caf14f6ce9ac
Paul Nettleton <paulnett7@hotmail.com>
2016-08-06 17:20:23 -0400

    Fixed links.

--- js/navbar.js
+++ js/navbar.js
@@ -2,12 +2,13 @@ document.write(
   "<ul id='nav'>\
     <img class='nav_logo' src='res/logo.svg'/>\
     <div class='nav_title'>\
-      <p style='font-size:30px;margin-bottom:0;margin-top:10px;''><b>CWRUbotix</b></p>\
-      <p style='font-size:16px;margin-top:0;'>Case Western Reserve University's Robotics Team</p>\
+      <p style='font-size:30px;margin-bottom:0;margin-top:10px;margin-left:0'><b>CWRUbotix</b></p>\
+      <p style='font-size:16px;margin-top:0;margin-left:0;margin-bottom:16px'>Case Western Reserve University's Robotics Team</p>\
     </div>\
     <li><a href='index.html'>Home</a></li>\
     <li><a href='projects.html'>Projects</a></li>\
     <li><a href='#members'>Members</a></li>\
-    <li><a href='contact.html>Contact</a></li>\
+    <li><a href='sponsorship.html'>Sponsorship</a></li>\
+    <li><a href='contact.html'>Contact</a></li>\
   </ul>"
 );
