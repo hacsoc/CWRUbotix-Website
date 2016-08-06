ca24aa67beeef6e7a2daf0b2814f500409425912
Paul Nettleton <paulnett7@hotmail.com>
2016-08-06 10:46:06 -0400

    Added a footer.

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,22 @@
 document.write(
-  ""
+  "<div id='footer'>\
+    <ul>\
+      <li id='contact'>\
+        <a href='contact.html'>Contact Us</a>\
+      </li>\
+      <li id='facebook'>\
+        <a href='https://www.facebook.com/cwrurobotics'>\
+          <img alt='facebook' src='res/facebook-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='twitter'>\
+        <a href='https://twitter.com/cwrubotix'>\
+          <img alt='twitter' src='res/twitter-logo.svg' />\
+        </a>\
+      </li>\
+    </ul>\
+    <p id='copyright'>\
+      Copyright 2016 CWRUbotix\
+    </p>\
+  </div>"
 );
