44b9020420043b7a4d16f2a0832395ba3b73d663
Paul Nettleton <paulnett7@hotmail.com>
2016-08-06 15:38:09 -0400

    Added some CSS.

--- js/navbar.js
+++ js/navbar.js
@@ -5,9 +5,9 @@ document.write(
       <p style='font-size:30px;margin-bottom:0;margin-top:10px;''><b>CWRUbotix</b></p>\
       <p style='font-size:16px;margin-top:0;'>Case Western Reserve University's Robotics Team</p>\
     </div>\
-    <li><a href='#home'>Home</a></li>\
-    <li><a href='#whatwedo'>Projects</a></li>\
+    <li><a href='index.html'>Home</a></li>\
+    <li><a href='projects.html'>Projects</a></li>\
     <li><a href='#members'>Members</a></li>\
-    <li><a href=''#contact'>Contact</a></li>\
+    <li><a href='contact.html'>Contact</a></li>\
   </ul>"
 );
