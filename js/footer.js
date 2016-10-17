810f74539de6565cc1f745d772150fe4fc1a36e5
Anno van den Akker <annovda@gmail.com>
2016-10-16 23:15:43 -0400

    Formatting changes, bootcamp added, navbar jumping fix, youtube fix, sponsors added

--- js/footer.js
+++ js/footer.js
@@ -5,22 +5,22 @@ document.write(
         <a href='contact.html'>Contact Us</a>\
       </li>\
       <li id='github'>\
-        <a href='https://github.com/cwruRobotics'>\
+        <a href='https://github.com/cwruRobotics' target=\"_blank\">\
           <img alt='GitHub' src='res/footer/github-logo.svg' />\
         </a>\
       </li>\
       <li id='facebook'>\
-        <a href='https://www.facebook.com/cwrurobotics'>\
+        <a href='https://www.facebook.com/cwrurobotics' target=\"_blank\">\
           <img alt='facebook' src='res/footer/facebook-logo.svg' />\
         </a>\
       </li>\
       <li id='twitter'>\
-        <a href='https://twitter.com/cwrubotix'>\
+        <a href='https://twitter.com/cwrubotix' target=\"_blank\">\
           <img alt='twitter' src='res/footer/twitter-logo.svg' />\
         </a>\
       </li>\
       <li id='youtube'>\
-        <a href='https://www.youtube.com/cwrubotix'>\
+        <a href='https://www.youtube.com/channel/UCFxYzjLEY09YFLx_UKSINIw' target=\"_blank\">\
           <img alt='YouTube' src='res/footer/youtube-icon.svg' />\
         </a>\
       </li>\
