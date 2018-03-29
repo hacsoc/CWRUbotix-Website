32eadb687396d03d4682abeaeb8d1a12c78ee0cf
Tyler Thieding <tyler@thieding.com>
2018-03-28 22:01:18 -0400

    Add hyperlinks to blog index page.

--- js/navbar.js
+++ js/navbar.js
@@ -8,7 +8,7 @@ document.write(
     <li><a href='index.html#main'>Home</a></li>\
     <li><a href='index.html#team'>Members</a></li>\
 	<li><a href='index.html#projects'>Projects</a></li>\
-    <li><a href='index.html#blog'>Blog</a></li>\
+    <li><a href='blog/index.html'>Blog</a></li>\
 	<li><a href='index.html#sponsorship'>Sponsorship</a></li>\
     <li><a href='index.html#contact'>Contact</a></li>\
   </ul>"
